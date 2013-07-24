fd4c44b6ec9959389bd36cd34eb462fda401d6d1
Aymeric Bois <aymeric.bois@franceretraite.fr>
2013-07-24 14:09:45 +0200

    Add navbarPosition feature

--- src/paginit.js
+++ src/paginit.js
@@ -1,109 +1,120 @@
 (function ($) {
-	  $.fn.paginIt = function (options) {
-		    var config = {
+    $.fn.paginIt = function (options) {
+        var config = {
           current: 0,
-			    matchingElement: 'article',
-			    width: '100%',
-			    cssPrefix: 'paginit-',
-			    prevContent: '&lt;',
-			    nextContent: '&gt;',
-			    // Requires jQuery UI
-			    slideEffect: true,
+          matchingElement: 'article',
+          width: '100%',
+          cssPrefix: 'paginit-',
+          navbarPosition: 'top',
+          prevContent: '&lt;',
+          nextContent: '&gt;',
+          // Requires jQuery UI
+          slideEffect: true,
           effectDuration: 500,
           beforeNext: null,
           beforePrev: null,
           AfterNext: null,
           AfterPrev: null
-		    };
-		    if (options) {
-			      $.extend(true, config, options);
-		    }
-		    return this.each(function () {
-			      $this = $(this);
-			      $this.addClass('paginit');
+        };
+        if (options) {
+            $.extend(true, config, options);
+        }
+        return this.each(function () {
+            $this = $(this);
+            $this.addClass('paginit');
 
-			      // Init
-			      elements = $this.find(config.matchingElement);
-			      nbElements = elements.length;
-			      current = config.current;
-			      currentElement = elements.eq(current).addClass(config.cssPrefix + 'current');
-			
-			      // Build html
-			      wrapper = $('<div>', {'class': 'paginit-wrapper'}).appendTo($this);
+            // Init
+            elements = $this.find(config.matchingElement);
+            nbElements = elements.length;
+            current = config.current;
+            currentElement = elements.eq(current).addClass(config.cssPrefix + 'current');
+
+            // Build html
+            wrapper = $('<div>', {'class': 'paginit-wrapper'}).appendTo($this);
             wrapperElements = $('<div>').appendTo(wrapper);
-			      elements.each(function () {
-			          $(this).appendTo(wrapperElements);
-			      });
-			      navbar = $('<nav>', {'class': config.cssPrefix+'navbar'}).prependTo(wrapper);
-			      prevElement = $('<span>', {'class': config.cssPrefix+'prev'}).html(config.prevContent).appendTo(navbar);
-			      selectElement = $('<select>', {'class': config.cssPrefix+'choices'}).appendTo(navbar);
-			      $(config.matchingElement).each(function (i,val) {
-				      $('<option>', {'value': i}).text($(val).attr('data-title') ? $(val).attr('data-title') : i+1).appendTo(selectElement);
-			      });
-			      selectElement.val(current);
-			      nextElement = $('<span>', {'class': config.cssPrefix+'next'}).html(config.nextContent).appendTo(navbar);
+            elements.each(function () {
+                $(this).appendTo(wrapperElements);
+            });
 
-			      // build css
-			      wrapper.css({
-				        'overflow': 'hidden',
+            // Navbar
+            navbar = $('<nav>', {'class': config.cssPrefix+'navbar'});
+            if(config.navbarPosition === "top")
+                navbar.prependTo(wrapper);
+            else if(config.navbarPosition === "bottom")
+                  navbar.appendTo(wrapper);
+            else {
+                navbar.prependTo(wrapper);
+                console.warn("'" + config.navbarPosition + "' value for navbarPosition is not available. Expected value are 'top' or 'bottom'.");
+            }
+            prevElement = $('<span>', {'class': config.cssPrefix+'prev'}).html(config.prevContent).appendTo(navbar);
+            selectElement = $('<select>', {'class': config.cssPrefix+'choices'}).appendTo(navbar);
+            $(config.matchingElement).each(function (i,val) {
+                $('<option>', {'value': i}).text($(val).attr('data-title') ? $(val).attr('data-title') : i+1).appendTo(selectElement);
+            });
+            selectElement.val(current);
+            nextElement = $('<span>', {'class': config.cssPrefix+'next'}).html(config.nextContent).appendTo(navbar);
+
+            // build css
+            wrapper.css({
+                'overflow': 'hidden',
                 'width': config.width,
                 'position': 'relative'
-			      });
-			      elements.css({
-				        'display': 'none',
-				        'width': config.width
-			      });
+            });
+            elements.css({
+                'display': 'none',
+                'width': config.width
+            });
             elements.eq(current).css({
-				        'display': 'block'
-			      });
-			      navbar.css({
-			          'width': config.width,
-			          'text-align': 'center'
-			      });
-			      nextElement.css({
-			          'display': 'inline-block',
+                'display': 'block'
+            });
+            navbar.css({
+                'width': config.width,
+                'text-align': 'center'
+            });
+            nextElement.css({
+                'display': 'inline-block',
                 'text-decoration': 'none',
                 'font-family': 'sans-serif',
                 'padding': '2px 5px',
                 'margin': '1px 3px',
                 'min-width': '10px',
                 'cursor': 'pointer'
-			      });
-			      prevElement.css({
-			          'text-decoration': 'none',
+            });
+            prevElement.css({
+                'text-decoration': 'none',
                 'font-family': 'sans-serif',
                 'padding': '2px 5px',
                 'margin': '1px 3px',
                 'min-width': '10px',
                 'cursor': 'pointer'
-			      });
-			      selectElement.css({
-			          'display': 'inline-block',
+            });
+            selectElement.css({
+                'display': 'inline-block',
                 'text-decoration': 'none',
                 'font-family': 'sans-serif',
                 'min-width': '100px'
-			      });
+            });
 
-			      if(current === 0) {
-			          prevElement.css({'opacity': 0.5, 'cursor': 'default'}).addClass(config.cssPrefix+'disabled');
-			      }
-			      if(current === nbElements-1) {
-			          nextElement.css({'opacity': 0.5, 'cursor': 'default'}).addClass(config.cssPrefix+'disabled');
-			      }
+            if(current === 0) {
+                prevElement.css({'opacity': 0.5, 'cursor': 'default'}).addClass(config.cssPrefix+'disabled');
+            }
+            if(current === nbElements-1) {
+                nextElement.css({'opacity': 0.5, 'cursor': 'default'}).addClass(config.cssPrefix+'disabled');
+            }
 
-			      // EVENTS
-			      selectElement.on('change', function () {
-				        var val = $(this).val();
-				        goTo(val);
-			      });
-			      nextElement.on('click', function () {
-				      next();
-			      });
-			      prevElement.on('click', function () {
-				      prev();
-			      });
-		    });
-	      function goTo(element) {
+            // EVENTS
+            selectElement.on('change', function () {
+                var val = $(this).val();
+                goTo(val);
+            });
+            nextElement.on('click', function () {
+              next();
+            });
+            prevElement.on('click', function () {
+              prev();
+            });
+        });
+        function goTo(element) {
             elements.eq(current).removeClass(config.cssPrefix + 'current').hide();
             if(config.slideEffect)
                 if(jQuery.ui)
@@ -121,10 +132,10 @@
             if(!hasNext())
                 nextElement.css({'opacity': 0.5, 'cursor': 'default'}).addClass(config.cssPrefix+'disabled');
             if(!hasPrev())
-				        prevElement.css({'opacity': 0.5, 'cursor': 'default'}).addClass(config.cssPrefix+'disabled');
-		    }
-		
-		    function next() {
+                prevElement.css({'opacity': 0.5, 'cursor': 'default'}).addClass(config.cssPrefix+'disabled');
+        }
+
+        function next() {
             if(hasNext()) {
                 if(typeof(config.beforeNext) === 'function')
                     if(config.beforeNext() !== false)
@@ -136,9 +147,9 @@
                 if(typeof(config.afterNext) === 'function')
                     config.afterNext();
             }
-		    }
-		
-		    function prev() {
+        }
+
+        function prev() {
             if(hasPrev()) {
                 if(typeof(config.beforePrev) === 'function')
                     if(config.beforePrev())
@@ -150,20 +161,20 @@
                 if(typeof(config.afterPrev) === 'function')
                     config.afterPrev();
             }
-		    }
-		
-		    function hasNext() {
-			      if(current>=nbElements-1) {
-				        return false;
-			      }
-			      return true;
-		    }
-		
-		    function hasPrev() {
-			      if(current<=0) {
-				        return false;
-			      }
-			      return true;
-		    }
+        }
+
+        function hasNext() {
+            if(current>=nbElements-1) {
+                return false;
+            }
+            return true;
+        }
+
+        function hasPrev() {
+            if(current<=0) {
+                return false;
+            }
+            return true;
+        }
     };
 })(jQuery);
